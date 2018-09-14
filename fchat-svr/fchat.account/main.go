ac8edb6e45c7ee56373cd081c4486bb1598f856d
gitfong <799618118@qq.com>
2018-09-14 02:09:10 -0400

    compile rpc protocol to .go

--- fchat-svr/fchat.account/main.go
+++ fchat-svr/fchat.account/main.go
@@ -16,7 +16,7 @@ func init() {
 }
 
 const (
-	accountSvrddr = ":9091"
+	accountSvrAddr = ":9091"
 )
 
 type accountServer struct{}
@@ -24,16 +24,16 @@ type accountServer struct{}
 func (s *accountServer) Register(ctx context.Context, in *rpcPb.RegisterReq) (*rpcPb.RegisterRsp, error) {
 	//time.Sleep(time.Second * 10)	//客户端会阻塞等待
 	fmt.Println("[accountSvr] on Register ", in.Account, in.Password)
-	return &rpcPb.RegisterRsp{RetCode: 0, Uid: 1, Desc: "注册成功"}, nil
+	return &rpcPb.RegisterRsp{RetCode: 0, Uid: 1, Desc: "register succeed!"}, nil
 }
 
 func (s *accountServer) Login(ctx context.Context, in *rpcPb.LoginReq) (*rpcPb.LoginRsp, error) {
 	fmt.Println("[accountSvr] on Login ", in.Account, in.Password)
-	return &rpcPb.LoginRsp{RetCode: 0, Uid: 1, Desc: "注册成功"}, nil
+	return &rpcPb.LoginRsp{RetCode: 0, Uid: 1, Desc: "login succeed!"}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", accountSvrddr)
+	lis, err := net.Listen("tcp", accountSvrAddr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
