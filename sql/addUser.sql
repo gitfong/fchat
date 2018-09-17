f350e12ac136e65579b3da2e945efc37525fba98
gitfong <799618118@qq.com>
2018-09-17 04:03:22 -0400

    modify sql script

--- sql/addUser.sql
+++
@@ -1,5 +0,0 @@
-use fchat;
-
-insert users(name,sex,age,tel)values('rose','girl',22,'15999999999');
-insert users(name,sex,age,tel)values('jack','boy',24,'15999999988');
-insert users(name,sex,age,tel)values('lily','girl',22,'15999999977');
--- sql/createAccountTB.sql
+++ sql/createAccountTB.sql
@@ -2,9 +2,9 @@ use fchat;
 
 create table accounts(
 	id int unsigned not null auto_increment primary key,
-	account varchar(16) not null,
+	account varchar(16) not null unique,
 	password varchar(16) not null,
 	nickName varchar(12) null default "",
-	registerTime datetime null,
-	lastLoginTime datetime null
-);
+	registerTime timestamp not null default now(),
+	lastLoginTime timestamp not null default now()
+)ENGINE=InnoDB DEFAULT CHARSET=gbk;
