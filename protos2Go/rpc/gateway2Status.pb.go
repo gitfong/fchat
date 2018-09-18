108bdbf9946dccb7f1d7a638265a76614307ac7e
gitfong <799618118@qq.com>
2018-09-18 05:36:18 -0400

    compile proto to go file

--- protos2Go/rpc/gateway2Status.pb.go
+++ protos2Go/rpc/gateway2Status.pb.go
@@ -23,6 +23,66 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+type Heartbeat2StatusReq struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Heartbeat2StatusReq) Reset()         { *m = Heartbeat2StatusReq{} }
+func (m *Heartbeat2StatusReq) String() string { return proto.CompactTextString(m) }
+func (*Heartbeat2StatusReq) ProtoMessage()    {}
+func (*Heartbeat2StatusReq) Descriptor() ([]byte, []int) {
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{0}
+}
+func (m *Heartbeat2StatusReq) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Heartbeat2StatusReq.Unmarshal(m, b)
+}
+func (m *Heartbeat2StatusReq) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Heartbeat2StatusReq.Marshal(b, m, deterministic)
+}
+func (dst *Heartbeat2StatusReq) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Heartbeat2StatusReq.Merge(dst, src)
+}
+func (m *Heartbeat2StatusReq) XXX_Size() int {
+	return xxx_messageInfo_Heartbeat2StatusReq.Size(m)
+}
+func (m *Heartbeat2StatusReq) XXX_DiscardUnknown() {
+	xxx_messageInfo_Heartbeat2StatusReq.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Heartbeat2StatusReq proto.InternalMessageInfo
+
+type Heartbeat2StatusRsp struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Heartbeat2StatusRsp) Reset()         { *m = Heartbeat2StatusRsp{} }
+func (m *Heartbeat2StatusRsp) String() string { return proto.CompactTextString(m) }
+func (*Heartbeat2StatusRsp) ProtoMessage()    {}
+func (*Heartbeat2StatusRsp) Descriptor() ([]byte, []int) {
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{1}
+}
+func (m *Heartbeat2StatusRsp) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Heartbeat2StatusRsp.Unmarshal(m, b)
+}
+func (m *Heartbeat2StatusRsp) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Heartbeat2StatusRsp.Marshal(b, m, deterministic)
+}
+func (dst *Heartbeat2StatusRsp) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Heartbeat2StatusRsp.Merge(dst, src)
+}
+func (m *Heartbeat2StatusRsp) XXX_Size() int {
+	return xxx_messageInfo_Heartbeat2StatusRsp.Size(m)
+}
+func (m *Heartbeat2StatusRsp) XXX_DiscardUnknown() {
+	xxx_messageInfo_Heartbeat2StatusRsp.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Heartbeat2StatusRsp proto.InternalMessageInfo
+
 type SignInReq struct {
 	Uid                  int32    `protobuf:"varint,1,opt,name=uid,proto3" json:"uid,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -34,7 +94,7 @@ func (m *SignInReq) Reset()         { *m = SignInReq{} }
 func (m *SignInReq) String() string { return proto.CompactTextString(m) }
 func (*SignInReq) ProtoMessage()    {}
 func (*SignInReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_gateway2Status_8ea4d4ecb6caf940, []int{0}
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{2}
 }
 func (m *SignInReq) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SignInReq.Unmarshal(m, b)
@@ -73,7 +133,7 @@ func (m *SignInRsp) Reset()         { *m = SignInRsp{} }
 func (m *SignInRsp) String() string { return proto.CompactTextString(m) }
 func (*SignInRsp) ProtoMessage()    {}
 func (*SignInRsp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_gateway2Status_8ea4d4ecb6caf940, []int{1}
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{3}
 }
 func (m *SignInRsp) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SignInRsp.Unmarshal(m, b)
@@ -119,7 +179,7 @@ func (m *UpdateStatusReq) Reset()         { *m = UpdateStatusReq{} }
 func (m *UpdateStatusReq) String() string { return proto.CompactTextString(m) }
 func (*UpdateStatusReq) ProtoMessage()    {}
 func (*UpdateStatusReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_gateway2Status_8ea4d4ecb6caf940, []int{2}
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{4}
 }
 func (m *UpdateStatusReq) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_UpdateStatusReq.Unmarshal(m, b)
@@ -164,7 +224,7 @@ func (m *UpdateStatusRsp) Reset()         { *m = UpdateStatusRsp{} }
 func (m *UpdateStatusRsp) String() string { return proto.CompactTextString(m) }
 func (*UpdateStatusRsp) ProtoMessage()    {}
 func (*UpdateStatusRsp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_gateway2Status_8ea4d4ecb6caf940, []int{3}
+	return fileDescriptor_gateway2Status_8a40c385834b3e03, []int{5}
 }
 func (m *UpdateStatusRsp) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_UpdateStatusRsp.Unmarshal(m, b)
@@ -192,6 +252,8 @@ func (m *UpdateStatusRsp) GetRetCode() int32 {
 }
 
 func init() {
+	proto.RegisterType((*Heartbeat2StatusReq)(nil), "rpcProtos.Heartbeat2StatusReq")
+	proto.RegisterType((*Heartbeat2StatusRsp)(nil), "rpcProtos.Heartbeat2StatusRsp")
 	proto.RegisterType((*SignInReq)(nil), "rpcProtos.SignInReq")
 	proto.RegisterType((*SignInRsp)(nil), "rpcProtos.SignInRsp")
 	proto.RegisterType((*UpdateStatusReq)(nil), "rpcProtos.UpdateStatusReq")
@@ -210,6 +272,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type StatusClient interface {
+	Heartbeat(ctx context.Context, in *Heartbeat2StatusReq, opts ...grpc.CallOption) (*Heartbeat2StatusRsp, error)
 	SignIn(ctx context.Context, in *SignInReq, opts ...grpc.CallOption) (*SignInRsp, error)
 	UpdateStatus(ctx context.Context, in *UpdateStatusReq, opts ...grpc.CallOption) (*UpdateStatusRsp, error)
 }
@@ -220,6 +283,15 @@ type statusClient struct {
 
 func NewStatusClient(cc *grpc.ClientConn) StatusClient {
 	return &statusClient{cc}
+}
+
+func (c *statusClient) Heartbeat(ctx context.Context, in *Heartbeat2StatusReq, opts ...grpc.CallOption) (*Heartbeat2StatusRsp, error) {
+	out := new(Heartbeat2StatusRsp)
+	err := c.cc.Invoke(ctx, "/rpcProtos.Status/Heartbeat", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *statusClient) SignIn(ctx context.Context, in *SignInReq, opts ...grpc.CallOption) (*SignInRsp, error) {
@@ -242,12 +314,31 @@ func (c *statusClient) UpdateStatus(ctx context.Context, in *UpdateStatusReq, op
 
 // StatusServer is the server API for Status service.
 type StatusServer interface {
+	Heartbeat(context.Context, *Heartbeat2StatusReq) (*Heartbeat2StatusRsp, error)
 	SignIn(context.Context, *SignInReq) (*SignInRsp, error)
 	UpdateStatus(context.Context, *UpdateStatusReq) (*UpdateStatusRsp, error)
 }
 
 func RegisterStatusServer(s *grpc.Server, srv StatusServer) {
 	s.RegisterService(&_Status_serviceDesc, srv)
+}
+
+func _Status_Heartbeat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Heartbeat2StatusReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StatusServer).Heartbeat(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/rpcProtos.Status/Heartbeat",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StatusServer).Heartbeat(ctx, req.(*Heartbeat2StatusReq))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _Status_SignIn_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -291,6 +382,10 @@ var _Status_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*StatusServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
+			MethodName: "Heartbeat",
+			Handler:    _Status_Heartbeat_Handler,
+		},
+		{
 			MethodName: "SignIn",
 			Handler:    _Status_SignIn_Handler,
 		},
@@ -304,22 +399,25 @@ var _Status_serviceDesc = grpc.ServiceDesc{
 }
 
 func init() {
-	proto.RegisterFile("gateway2Status.proto", fileDescriptor_gateway2Status_8ea4d4ecb6caf940)
+	proto.RegisterFile("gateway2Status.proto", fileDescriptor_gateway2Status_8a40c385834b3e03)
 }
 
-var fileDescriptor_gateway2Status_8ea4d4ecb6caf940 = []byte{
-	// 207 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_gateway2Status_8a40c385834b3e03 = []byte{
+	// 247 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x49, 0x4f, 0x2c, 0x49,
 	0x2d, 0x4f, 0xac, 0x34, 0x0a, 0x2e, 0x49, 0x2c, 0x29, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
-	0x17, 0xe2, 0x2c, 0x2a, 0x48, 0x0e, 0x00, 0xb1, 0x8a, 0x95, 0x64, 0xb9, 0x38, 0x83, 0x33, 0xd3,
-	0xf3, 0x3c, 0xf3, 0x82, 0x52, 0x0b, 0x85, 0x04, 0xb8, 0x98, 0x4b, 0x33, 0x53, 0x24, 0x18, 0x15,
-	0x18, 0x35, 0x58, 0x83, 0x40, 0x4c, 0x25, 0x4b, 0xb8, 0x74, 0x71, 0x81, 0x90, 0x04, 0x17, 0x7b,
-	0x51, 0x6a, 0x89, 0x73, 0x7e, 0x4a, 0x2a, 0x54, 0x09, 0x8c, 0x2b, 0x24, 0xc4, 0xc5, 0x92, 0x92,
-	0x5a, 0x9c, 0x2c, 0xc1, 0xa4, 0xc0, 0xa8, 0xc1, 0x19, 0x04, 0x66, 0x2b, 0x59, 0x73, 0xf1, 0x87,
-	0x16, 0xa4, 0x24, 0x96, 0xa4, 0x42, 0xac, 0xc6, 0x6a, 0xbe, 0x90, 0x18, 0x17, 0x5b, 0x31, 0x58,
-	0x1a, 0xac, 0x95, 0x35, 0x08, 0xca, 0x53, 0xd2, 0x46, 0xd3, 0x8c, 0xcf, 0x76, 0xa3, 0x2e, 0x46,
-	0x2e, 0x36, 0x88, 0x3a, 0x21, 0x33, 0x2e, 0x36, 0x88, 0x7b, 0x85, 0x44, 0xf4, 0xe0, 0x9e, 0xd4,
-	0x83, 0xfb, 0x50, 0x0a, 0x8b, 0x68, 0x71, 0x81, 0x12, 0x83, 0x90, 0x07, 0x17, 0x0f, 0xb2, 0x7d,
-	0x42, 0x52, 0x48, 0xea, 0xd0, 0x7c, 0x21, 0x85, 0x53, 0x0e, 0x64, 0x52, 0x12, 0x1b, 0x38, 0x88,
-	0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x87, 0x6b, 0xcd, 0x89, 0x7a, 0x01, 0x00, 0x00,
+	0x17, 0xe2, 0x2c, 0x2a, 0x48, 0x0e, 0x00, 0xb1, 0x8a, 0x95, 0x44, 0xb9, 0x84, 0x3d, 0x52, 0x13,
+	0x8b, 0x4a, 0x92, 0x52, 0x13, 0x4b, 0xa0, 0x8a, 0x82, 0x52, 0x0b, 0xb1, 0x0a, 0x17, 0x17, 0x28,
+	0xc9, 0x72, 0x71, 0x06, 0x67, 0xa6, 0xe7, 0x79, 0xe6, 0x05, 0xa5, 0x16, 0x0a, 0x09, 0x70, 0x31,
+	0x97, 0x66, 0xa6, 0x48, 0x30, 0x2a, 0x30, 0x6a, 0xb0, 0x06, 0x81, 0x98, 0x4a, 0x96, 0x70, 0xe9,
+	0xe2, 0x02, 0x21, 0x09, 0x2e, 0xf6, 0xa2, 0xd4, 0x12, 0xe7, 0xfc, 0x94, 0x54, 0xa8, 0x12, 0x18,
+	0x57, 0x48, 0x88, 0x8b, 0x25, 0x25, 0xb5, 0x38, 0x59, 0x82, 0x49, 0x81, 0x51, 0x83, 0x33, 0x08,
+	0xcc, 0x56, 0xb2, 0xe6, 0xe2, 0x0f, 0x2d, 0x48, 0x49, 0x2c, 0x49, 0x85, 0xbb, 0x01, 0xd3, 0x7c,
+	0x21, 0x31, 0x2e, 0xb6, 0x62, 0xb0, 0x34, 0x58, 0x2b, 0x6b, 0x10, 0x94, 0xa7, 0xa4, 0x8d, 0xa6,
+	0x19, 0x9f, 0xed, 0x46, 0x37, 0x19, 0xb9, 0xd8, 0x20, 0xea, 0x84, 0x7c, 0xb9, 0x38, 0xe1, 0xbe,
+	0x14, 0x92, 0xd3, 0x83, 0x87, 0x8a, 0x1e, 0x96, 0x20, 0x91, 0xc2, 0x2b, 0x5f, 0x5c, 0xa0, 0xc4,
+	0x20, 0x64, 0xc6, 0xc5, 0x06, 0xf1, 0xbe, 0x90, 0x08, 0x92, 0x5a, 0x78, 0x80, 0x49, 0x61, 0x11,
+	0x05, 0xeb, 0xf3, 0xe0, 0xe2, 0x41, 0x76, 0xbe, 0x90, 0x14, 0x92, 0x3a, 0xb4, 0x40, 0x91, 0xc2,
+	0x29, 0x07, 0x32, 0x29, 0x89, 0x0d, 0x1c, 0xbf, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc1,
+	0x0b, 0xe8, 0x9c, 0xf7, 0x01, 0x00, 0x00,
 }
